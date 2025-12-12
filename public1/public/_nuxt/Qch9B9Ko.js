9f2a7f013d1fb6c27326bfe5d75cc0d04052e0bc
Mikhail_code <arhipovma@yandex.ru>
2025-12-12 10:32:22 +0300

    public1

---
+++ public1/public/_nuxt/Qch9B9Ko.js
@@ -0,0 +1,1 @@
+const e={chart:{type:"pie"},title:{text:"График тип pie"},plotOptions:{pie:{dataLabels:{style:{color:"#333333",fontSize:"14px",fontWeight:"bold"}}}},series:[{data:[{name:"Сектор за 2023",y:40,color:"#5890F8"},{name:"Сектор за 2024",y:30,color:"#ADB6CE"},{name:"Сектор за 2025",y:55,color:"#DFF0EA"}]}]};export{e as default};
