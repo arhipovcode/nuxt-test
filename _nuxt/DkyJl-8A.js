b09af354669663c901949bf4f673dd18678844ca
arhipovcode <arhipovcode@users.noreply.github.com>
2025-12-12 09:54:12 +0000

    deploy: ec2ec8d39396f3da0475d369ecdb674b6f2e68d1

---
+++ _nuxt/DkyJl-8A.js
@@ -0,0 +1,7 @@
+const e=[{id:"1",desc:'В корневом уровне настроек, добавить объект "credits" с параметром "enabled: false"',code:{credits:{enabled:!1}}},{id:"2",desc:`В объекте "tooltip" добавить функцию "formatter", <br>
+               Также можно добавить любые данные. <br>
+               Опции: <br>
+                - <b>useHTML</b> – включает добавление HTML <br>
+                - <b>followPointer</b> – tooltip следует за курсором мыши <br>
+                - <b>outside</b> – выходит за пределы графика (контейнер имеет overflow: hidden) <br>
+                - <b>shadow</b> – отключает тень`,code:{tooltip:{useHTML:!0,followPointer:!0,outside:!0,shadow:!1,backgroundColor:"transparent",padding:0,formatter:function(){return`<span class='my-class-tooltip'>Имя серии: ${this.name} - значение: ${this.y}</span>`}}}},{id:"3",desc:'В корне, добавить объект "legend" с параметрами "enabled: false"',code:{legend:{enabled:!1}}}];export{e as default};
